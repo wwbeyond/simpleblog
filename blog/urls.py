a63129bf3cae7292788aed586959ba493db928f1
wwbeyond <wwbeyond@me.com>
2016-06-23 14:18:06 +1000

    'post_lists'

--- blog/urls.py
+++ blog/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'',include('blog.urls')),
+    url(r'^$',views.post_list),
 ]
