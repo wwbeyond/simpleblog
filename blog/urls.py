89da8ac741a83b1c4c724a9dfb1760f60b849131
wwbeyond <wwbeyond@me.com>
2016-12-19 11:00:56 +1100

    Test html

--- blog/urls.py
+++ blog/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.post_list),
-    url(r'^post/(?P<pk>[0-9]+)/$',views.post_detail ),
-    url(r'^post/new/$',views.post_new,name='post_new'),
+    url(r'^$',views.test, name='post_list'),
+#    url(r'^post/(?P<pk>[0-9]+)/$',views.post_detail ),
+#    url(r'^post/new/$',views.post_new,name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,19 +10,22 @@ def post_list(request):
     posts=Post.objects.filter(published_date__isnull=False).order_by('-published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
 
+def test(request):
+    return  render(request, 'blog/test1.html',{})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request, 'blog/post_detail.html',{'post':post})
 
-def post_new(request):
-  if request.method=="POST":
-    form=PostForm(request.POST)
-    if form.is_valid():
-      post=form.save(commit=False)
-      post.author=request.user
-      post.save()
-      return redirect('blog.views.post_detail',pk=post.pk)
-  else:
-     form=PostForm()
-  return render(request,'blog/post_edit.html',{"form":form})
+# def post_new(request):
+#   if request.method=="POST":
+#     form=PostForm(request.POST)
+#     if form.is_valid():
+#       post=form.save(commit=False)
+#       post.author=request.user
+#       post.save()
+#       return redirect('blog.views.post_detail',pk=post.pk)
+#   else:
+#      form=PostForm()
+#   return render(request,'blog/post_edit.html',{"form":form})
 
